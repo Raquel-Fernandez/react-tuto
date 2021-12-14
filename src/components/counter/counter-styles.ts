d2d57e8a25a779883226ed3ed31be81dceca11cb
Raquel-Fernandez <81975272+Raquel-Fernandez@users.noreply.github.com>
2021-12-14 12:24:26 +0100

    Añadiendo contador a la página de Productos
    Añadido contador que incrementa y decrementa un número a la página de productos.
    Se han añadido estilos a la página de productos y al contador.

---
+++ src/components/counter/counter-styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles: any = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }, 
+  Button: {
+    fontSize: '20px !important'
+  }
+});
+
+export default useStyles;
